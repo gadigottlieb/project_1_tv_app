c5641c504d822a7bd1d90479bcc4642bc8e6a02c
Gadi Gottlieb <gadigottlieb@gmail.com>
2014-07-14 18:56:33 -0400

    Added some style, also added a variable for my API Key.

--- app/models/tvdb.rb
+++ app/models/tvdb.rb
@@ -2,14 +2,14 @@ class TVDB
 
 # Searches TV Shows By Title
   def self.search_show_title(tv_info)
-    Tmdb::Api.key("02bf83da8680170802ffbae8e4b6f5f4")
+    Tmdb::Api.key(ENV["TMDB_API_KEY"])
     find_show = Tmdb::TV.find(tv_info)
     id = find_show[0].id
     tv_show = Tmdb::TV.detail(id)
   end
 # Search Actors By TV Show
   def self.search_actors_show(actor_info)
-    Tmdb::Api.key("02bf83da8680170802ffbae8e4b6f5f4")
+    Tmdb::Api.key(ENV["TMDB_API_KEY"])
     find_show = Tmdb::TV.find(actor_info)
     id = find_show[0].id
     actor_show = Tmdb::TV.cast(id)
